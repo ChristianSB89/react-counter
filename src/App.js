81625cd00a7095ba3c4a22c317add2c31d94d92c
Christian SB <christiansb.kodehode@gmail.com>
2022-08-25 19:40:27 +0200

    Configure React Counter app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [count = setInterval, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is {count} </h1>
+      <button onClick={() => setCount(count + 1)}>Add One</button>
+      <button onClick={() => setCount(count - 1)}>Remove One</button>
     </div>
   );
 }
